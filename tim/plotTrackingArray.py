13ce59199cf5f970bda77105e56cf9c84d1df487
Tim Gaggstatter <tgaggstatter@tgaggstatter-ld1.cl.gemini.edu>
2018-06-19 19:30:28 -0400

    plotTrackingArray: Added reading vali and plotting modes 'full' and 'eng'

--- tim/plotTrackingArray.py
+++ tim/plotTrackingArray.py
@@ -21,10 +21,12 @@ import numpy as np
 import pdb
 import os
 
-sys.path.append('../')
+sys.path.insert(0,'../')
+sys.path.append('..')
+
 from swglib.export import DataManager, get_exporter
 
-ENABLE_CACHING = False #TODO: Enable sample set for caching
+ENABLE_CACHING = True #TODO: Enable sample set for caching
     
 LEGEND_LOCATION = 'lower left'
 TZ = 'America/Santiago'
@@ -34,6 +36,7 @@ MARKERSIZE = 2.5
 
 TCS_SYSTEM = 'tcs'
 TCS_CA = 'tcs:drives:driveMCS.VALA'
+TCS_CAI = 'tcs:drives:driveMCS.VALI'
 MCS_SYSTEM = 'mcs'
 MCS_CA = 'mc:followA.J'
 
@@ -41,6 +44,8 @@ MCS_CA = 'mc:followA.J'
 EXEC_TIME_MODE = 'execTime'
 POS_DIFF_MODE = 'posDiff' #shortcut for rawDiff -cols 4,5
 VEL_MODE = 'vel'
+FULL_MODE = 'full'
+ENG_MODE = 'eng' #engineer mode uses VALI array from TCS
 
 PERIOD_MODE = 'period'
 RAW_MODE = 'raw'
@@ -53,7 +58,43 @@ CAMON_DIFF_MODE = 'camonDiff'
 FollowArray = namedtuple('FollowArray', 'timestamp now targetTime trackId azPos elPos diff')
 ZoneArray = namedtuple('ZoneArray', 'title begin end color')
 Limits = namedtuple('Limits', 'lower upper')
+DebugArray = namedtuple('DebugArray', 'timestamp now targetTime deltaApplyT period sem1 sem2 dmdCnt dmdCorr dmdLowCorr dmdHighCorr flCnt available azPos elPos')
 
+'''
+** Element 0  = Time at which commands is sent to MCS
+** Element 1  = traw from Fast Loop
+** Element 2  = Tick from Fast Loop
+** Element 3  = Time to execute demand since calculation (tick - traw)
+** Element 4  = Time between FastLoop executions
+** Element 5  = Time it takes to take and release reading semaphore on tcsFastLoop
+** Element 6  = Time it takes to take and release writing semaphore on tcsFastLoop
+** Element 7  = Demand Number
+** Element 8  = Tick Correction Number
+** Element 9  = correction under 20[ms]
+** Element 10  = correction over 80[ms]
+** Element 11  = Fast Loop Fault counter
+** Element 12  = Available
+** Element 13  = demanded Azimuth
+** Element 14  = demanded Elevation
+*/
+
+    FLinfo[0] = tnow;
+    FLinfo[1] = traw;
+    FLinfo[2] = tick;
+    FLinfo[3] = applyDeltaT;
+    FLinfo[4] = deltaTraw;
+    FLinfo[5] = deltaTick;
+    FLinfo[6] = dtGetTelRD;
+    FLinfo[7] = dmd_cnt;
+    FLinfo[8] = corr_cnt;
+    FLinfo[9] = corr_02;
+    FLinfo[10] = corr_08;
+    FLinfo[11] = flt_cnt;
+    FLinfo[12] = 0.0;
+    FLinfo[13] = azdef * R2D;
+    FLinfo[14] = eldef * R2D;
+
+'''
 
 # Site should be either 'cp' or 'mk'
 SITE = 'cp'
@@ -80,7 +121,7 @@ def parse_args():
     parser.add_argument('-scale',   '--scale',        dest='scale',  type=float,  default=1.0, help='Scale to be applied to data')
     parser.add_argument('-cols',   '--columns',        dest='cols',          default='4,5', help='Columns to be plotted')
     parser.add_argument('-axis',   '--axis',        dest='axis',          default='az', help='Axis to be plotted, can be az or el')
-    parser.add_argument('-time',   '--time_range',        dest='time_rng',    default=None, help='Specific time range format: HH:MM-HH:MM')
+    parser.add_argument('-time',   '--time_range',        dest='time_rng',    default=None, help='Specific time range format: HHMM-HHMM')
     
     
     
@@ -91,7 +132,7 @@ def parse_args():
 
     if ENABLE_CACHING: #Uses swglib
         if args.eng_mode:
-            args.time_rng = '800-2000'
+            args.time_rng = '0800-2000'
         else:
             if not args.time_rng:
                 args.time_rng = '2000-0800'
@@ -211,6 +252,7 @@ def producer(filename):
             generationTime = fromtimestampTz(float(row[1]))
             receptionTime = strptimeTz(row[0][:-3], True)
             targetTime = fromtimestampTz(float(row[2]))
+            #print row            
             yield FollowArray(receptionTime, generationTime,\
                  targetTime,  fromtimestampTz(float(row[3])), float(row[4]), float(row[5]), targetTime-receptionTime \
                  )
@@ -219,34 +261,36 @@ def producerH(pv, dateStr, timeRangeStr):
     
     begin, end = getTimes(dateStr, timeRangeStr)
     print 'Times:', begin, end
-    dm = DataManager(get_exporter(SITE.upper()), root_dir='/tmp/rcm') 
+    dm = DataManager(get_exporter(SITE.upper()), root_dir='/tmp/rcm/') 
     data = dm.getData(TCS_CA, begin, end)
 
     for val in data:
             receptionTime = localizeNp64Tz(val[0])
             generationTime = fromtimestampTz(val[1])
             targetTime = fromtimestampTz(val[2])
-                        
+            #print val[0], 'vs', receptionTime
+            #sys.exit()            
             yield FollowArray(receptionTime, generationTime, targetTime, val[3], val[4], val[5], targetTime-receptionTime)
             
     #TODO: Return generator
     #TODO: Implement timezone awareness on the swglib side
     
-    '''with open(filename) as source:
-        reader = csv.reader(source, delimiter='\t')
-        # Skip the headers
-        next(reader); next(reader); next(reader); next(reader)
 
-        for row in reader:
-            if row[0].startswith('#') or row[1].startswith('#'):
-                continue
-            generationTime = fromtimestampTz(float(row[1]))
-            receptionTime = strptimeTz(row[0][:-3], True)
-            targetTime = fromtimestampTz(float(row[2]))
-            yield FollowArray(receptionTime, generationTime,\
-                 targetTime,  fromtimestampTz(float(row[3])), float(row[4]), float(row[5]), targetTime-receptionTime \
-                 )
-'''
+#DebugArray = namedtuple('DebugArray', 'timestamp now targetTime deltaApplyT period sem1 sem2 dmdCnt dmdCorr dmdLowCorr dmdHighCorr flCnt available azPos elPos')
+def producerE(pv, dateStr, timeRangeStr):
+    
+    begin, end = getTimes(dateStr, timeRangeStr)
+    print 'Times:', begin, end
+    dm = DataManager(get_exporter(SITE.upper()), root_dir='/tmp/rcm/') 
+    data = dm.getData(TCS_CAI, begin, end)
+    for val in data:
+            receptionTime = localizeNp64Tz(val[0])
+            generationTime = fromtimestampTz(val[1])
+            targetTime = fromtimestampTz(val[2]) 
+            yield DebugArray(receptionTime, generationTime, targetTime, val[4], val[5], val[6], val[7],val[8],val[9],val[10],val[11],val[12],val[13],val[14],val[15])
+
+
+
 def readZonesFromFile(begin, end):
     markedZones = []
     try:
@@ -282,7 +326,7 @@ def plotPeriod():
     Here we are measuring periodicity between executions
     and periodicity (or duration) between consecutive targetTime calculations
     """
-    flw_producer = producer(args.data_path)
+    flw_producer = producerH(TCS_CA, args.date, args.time_rng)
     
     first = flw_producer.next()
     diffNow_lst, diffTarget_lst = list(), list()
@@ -352,7 +396,7 @@ def plotRaw(diff=False):
     Plots the data as it is, if diff flag is true it subtracts 2 consecutive
     values. [a b c] diff >> [b-a c-b]
     '''
-    flw_producer = producer(args.data_path)
+    flw_producer = producerH(TCS_CA, args.date, args.time_rng)
     i,j = getIndexes(args.cols)
     
     timeBase, aLst, bLst = list(), list(), list()
@@ -386,7 +430,7 @@ def plotRaw(diff=False):
     plt.show()
 
 def plotPosDiff():
-    flw_producer = producer(args.data_path)
+    flw_producer = producerH(TCS_CA, args.date, args.time_rng)
 
     firstVal = flw_producer.next()
     outliersInPeriod = 0
@@ -497,13 +541,181 @@ def plotVel():
 
     #ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%d/%m %H:%M:%S.%f"))
 
-    #plt.gcf().autofmt_xdate()
-    plt.setp(ax1.get_xticklabels(), fontsize=9, visible=False)
+    plt.gcf().autofmt_xdate()
+    #TODO: Special plot settings need to care about timezone
+    """plt.setp(ax1.get_xticklabels(), fontsize=9, visible=False)
     ax2.xaxis.set_major_locator(ticker.MaxNLocator(10))
     ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M:%S.%f"))
     ax2.xaxis.set_minor_locator(ticker.MaxNLocator(200))
     plt.setp(ax2.get_xticklabels(), fontsize=9, rotation=20, ha='right')
-    plt.show()    
+"""    
+    plt.show() 
+
+def plotFull():
+    '''
+    Ignacio and his 10.000 plots on one graph haha
+    '''   
+    if ENABLE_CACHING:
+        flw_producer = producerH(TCS_CA, args.date, args.time_rng)
+    else:
+        flw_producer = producer(args.data_path)
+
+    diff_lst = list()
+    exec_lst = list()
+    dDmd_lst = list()
+    prevTimestamp = flw_producer.next().now
+    prevTarget = flw_producer.next().targetTime
+    prevElPos = flw_producer.next().elPos
+    outliersInPeriod = 0
+    periodLimits = Limits(-1,1)
+
+    #FollowArray = namedtuple('FollowArray', 'timestamp now targetTime trackId azPos elPos diff')
+    # when = datetime.strptime("2018-04-26 11:18", "%Y-%m-%d %H:%M")
+
+    for dp in flw_producer:
+        #print dp.targetTime-dp.now
+        #print dp.diff.total_seconds()
+        # tBetweenSamples = (dp.now-prevTimestamp).total_seconds()
+        tBetweenSamples = dp.now-prevTimestamp
+        # tBetweenTargets = dp.targetTime-prevTarget
+        tBetweenTargets = dp.targetTime - dp.now
+        dDemands = dp.elPos - prevElPos
+        diff_lst.append((dp.timestamp, tBetweenSamples.total_seconds()*1000.0))
+        exec_lst.append((dp.timestamp, tBetweenTargets.total_seconds()*1000.0))
+        dDmd_lst.append((dp.timestamp, dDemands*3600))
+        prevTimestamp = dp.now
+        prevTarget = dp.targetTime
+        prevElPos = dp.elPos
+        # if(when<dp.timestamp):
+            # if (tBetweenSamples > 1.0):
+                # print "tBetweenSamples={0} at GEA time: {1}".format(tBetweenSamples, dp.timestamp)
+            # else:
+                # diff_lst.append((dp.timestamp, tBetweenSamples*1000.0))
+            # exec_lst.append((dp.timestamp, dp.azPos*1000.0))
+        # prevTimestamp = dp.now
+        #if tBetweenSamples > periodLimits.upper or tBetweenSamples < periodLimits.lower:
+        #    print "Period out of limits: {0} on date: {1}".format(tBetweenSamples, dp.timestamp)
+        #    outliersInPeriod += 1
+
+
+    print "Last read line with date:", dp.timestamp
+
+    if args.system=='both':
+        mcs_lst = list()
+        mcs_producer = producerMCS(args.mcs_data_path)
+        for dp in mcs_producer:
+            mcs_lst.append((dp.timestamp, dp.pmacError*3600))
+
+    # PLOTTING SECTION
+    # Plot 1
+    diffTime,diffVal=zip(*diff_lst)
+
+    ax1 = plt.subplot(411)
+    plt.title("TCS-MCS Communication Analysis: {0} data from {1}".format(args.system, args.date))
+    ax1.plot(diffTime, diffVal, "b.")
+    ax1.grid(True)
+    ax1.tick_params("y", colors="b")
+    ax1.set_ylabel("Diff timeNow [ms]", color="b")
+    ax1.set_ylim(-200, 500)
+
+    if outliersInPeriod > 0:
+        #ax1.set_ylim(*periodLimits)
+        print "Fixed scaling due to outliers"
+    else:
+        print "Rescaling Period axis due to no outliers in sample set"
+    #ax1.set_yticks(np.arange(-0.05, 0.05, step=0.005))
+    when = datetime.strptime("2018-04-18 09:45", "%Y-%m-%d %H:%M")
+    #ax1.axvspan(when, when+timedelta(minutes=230), facecolor='0.2', alpha=0.3)
+
+    # Plot 2
+    execTime, execVal=zip(*exec_lst)
+
+    ax2 = plt.subplot(412, sharex=ax1)
+    ax2.grid(True)
+    ax2.plot(execTime, execVal, "r.")
+    ax2.tick_params("y", colors="r")
+    ax2.set_ylabel("Tick - tnow [ms]", color="r")
+    ax2.set_ylim(-10, 60)
+
+    if args.system=='both':
+        # Plot 3
+        pmacTime, pmacVal=zip(*mcs_lst)
+
+        ax4 = plt.subplot(413, sharex=ax1)
+        ax4.grid(True)
+        ax4.plot(pmacTime, pmacVal, "r-")
+        ax4.tick_params("y", colors="r")
+        ax4.set_ylabel("Elevation Pmac Error [arcsec]", color="r")
+        ax4.set_ylim(-15, 15)
+
+    # Plot 4
+    dmdTime, diffDmd=zip(*dDmd_lst)
+
+    ax3 = plt.subplot(414, sharex=ax1)
+    ax3.plot(dmdTime, diffDmd, "g.-")
+    ax3.grid(True)
+    ax3.tick_params("y", colors="b")
+    ax3.set_ylabel("Diff EL Demand [arcsec]", color="b")
+    ax3.set_ylim(-10, 10)
+    """
+    plt.setp(ax1.get_xticklabels(), fontsize=9, visible=False)
+    plt.setp(ax2.get_xticklabels(), fontsize=9, visible=False)
+    if args.system=='both':        
+        plt.setp(ax4.get_xticklabels(), fontsize=9, visible=False)
+    ax3.xaxis.set_major_locator(ticker.MaxNLocator(10))
+    ax3.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%d/%m %H:%M:%S.%f"))
+    ax3.xaxis.set_minor_locator(ticker.MaxNLocator(200))
+    plt.setp(ax3.get_xticklabels(), fontsize=9, rotation=20, ha='right')
+    plt.gca().xaxis_date(TZ)
+#    pdb.set_trace()
+    print "Number of detected period outliers: {0}".format(outliersInPeriod)
+
+    ax2.xaxis_date(TZ)    
+    ax3.xaxis_date(TZ)
+    """
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+
+def plotEng():
+    '''
+    Here we are using VALI
+    '''   
+    if ENABLE_CACHING:
+        flw_producer = producerE(TCS_CAI, args.date, args.time_rng)
+    else:
+        print "Not implemented, use caching"
+        sys.exit()
+
+    timebase = list()
+    delta_lst = list()
+    period_lst = list()
+    for dp in flw_producer:
+        timebase.append(dp.timestamp)
+        delta_lst.append(dp.deltaApplyT)
+        period_lst.append(dp.period)
+
+    fig, ax1 = plt.subplots() 
+    ax1 = plt.subplot(211)
+    plt.title("TCS-MCS Period/DeltaT Analysis: {0} data from {1}".format(args.system, args.date))
+    ax1.plot(timebase, delta_lst, "r.-", markersize=MARKERSIZE)
+    ax1.grid(True)
+    ax1.tick_params("y", colors="r")
+    ax1.set_ylabel("DeltaT[s.]", color="r")
+
+
+    ax2 = plt.subplot(212, sharex=ax1)
+    ax2.plot(timebase, period_lst, "b.-", markersize=MARKERSIZE)
+    ax2.grid(True)
+    ax2.tick_params("y", colors="b")
+    ax2.set_ylabel("Period[s.]", color="b")
+    
+    addZones(ax1, timebase[0],timebase[-1])
+    addZones(ax2, timebase[0],timebase[-1])
+
+    plt.gcf().autofmt_xdate()
+    plt.show() 
+
 
 # -----------------------------------------------------------------------------
 # ------------------------------------ MAIN -----------------------------------
@@ -516,7 +728,7 @@ if args.mode == EXEC_TIME_MODE :
     you can chooose between tcs, mcs or both.
     '''
     
-    flw_producer = producer(args.data_path)
+    flw_producer = producerH(TCS_CA, args.date, args.time_rng)
     
     diff_lst = list()
     for dp in flw_producer:
@@ -565,6 +777,11 @@ elif args.mode == PERIOD_MODE:
 elif args.mode == VEL_MODE:
     plotVel()
    
+elif args.mode == FULL_MODE:
+    plotFull()
+
+elif args.mode == ENG_MODE:
+    plotEng()
    
 else:
     print "Mode not found, check spelling"
