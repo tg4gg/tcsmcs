95845ff5b95fe0a6dabf822666f37345562964b1
tgaggstatter <tgaggstatter@gemini.edu>
2020-04-15 09:49:33 -0400

    Added support for MK, show max-min also

--- timeAudit/timeAnalysis.py
+++ timeAudit/timeAnalysis.py
@@ -12,16 +12,25 @@ import pdb
 import matplotlib.pyplot as plt
 from collections import namedtuple
 
-SITE = 'CP'
-start_date = datetime(2020, 4, 8, 17, 00)
-end_date =   datetime(2020, 4, 9, 5, 00)
-systems = ['tc1', 'mc1', ]
+SITE = 'MK'
+start_date = datetime(2020, 4, 15, 2, 30)
+end_date =   datetime(2020, 4, 15, 4, 30)
+
+
 SystemData = namedtuple('SystemData', 'name times vals')
 
 if SITE == 'CP':  # Archiver returns UTC dates, need to cover for that
     UTC_OFFSET = timedelta(hours=4)
     TZ = 'America/Santiago'
+    DB = 'sbflab'
+    systems = ['tc1', 'mc1', ]
+    # systems = ['tc1', ]
 
+if SITE == 'MK':
+    UTC_OFFSET = timedelta(hours=10)
+    TZ = 'Pacific/Honolulu'
+    DB = 'temporary'
+    systems = ['tcs',]
 
 def retrieveAllData():
     """" Gets the data for each system and returns an easy to plot list"""
@@ -30,7 +39,7 @@ def retrieveAllData():
     for sys in systems:
         start = datetime.now()
         print(start, 'Retrieving values for ta:' + sys + ':diff')
-        sys_data = list(dm.getData('ta:' + sys + ':diff', start=start_date + UTC_OFFSET, end=end_date + UTC_OFFSET))
+        sys_data = list(dm.getData('ta:' + sys + ':diff', start=start_date + UTC_OFFSET, end=end_date + UTC_OFFSET, db=DB))
         print("Retrieved", len(sys_data), "values for", sys, "elapsed time ", datetime.now() - start)
         data_unzip = list(zip(*sys_data))  # data[0] is (datetime, val)
         if len(data_unzip) > 0:
@@ -38,16 +47,17 @@ def retrieveAllData():
             #periodicity = [abs((sys_data.vals[i+1]-sys_data.vals[i])) for i in range(len(sys_data.vals)-1)]
             #[if period  for period in periodicity]
             #pdb.set_trace()
+            print("Max val:", max(sys_data.vals), " Min val:", min(sys_data.vals))
             ret.append(sys_data)
         else:
-            print("No data found for {0} in the specified period.".format(sys))
+            print("No data found for {0} in the specified period.".format(sys))  # TODO: Warning
     return ret
 
 
 data = retrieveAllData()
 
 fig, ax1 = plt.subplots()
-plt.title("Time differences - ta is running as VME IOC connected to the Timebus, mc1 is down")
+plt.title("Time differences - ta is running as VME IOC connected to the Timebus")
 for sys in data:
     ax1.plot(sys.times, sys.vals, ".",label=sys.name)
 ax1.grid(True)
